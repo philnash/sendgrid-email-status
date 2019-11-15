f30f0c9ed9d1621e0eb403804091de5a91477900
Phil Nash <philnash@gmail.com>
2019-11-15 15:11:42 +1100

    Generates Messages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :emails, only: [:new, :create, :show]
+  get '/emails', to: redirect('/')
+  root to: 'emails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
